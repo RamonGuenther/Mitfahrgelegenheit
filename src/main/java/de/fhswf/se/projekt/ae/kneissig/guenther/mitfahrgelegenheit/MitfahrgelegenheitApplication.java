801f0ecc5234bf9926fb87dc8c1651c6658bff51
Ramon <guenther.ramonantonio@fh-swf.de>
2022-02-14 15:03:26 +0100

    Projektstart

---
+++ src/main/java/de/fhswf/se/projekt/ae/kneissig/guenther/mitfahrgelegenheit/MitfahrgelegenheitApplication.java
@@ -0,0 +1,13 @@
+package de.fhswf.se.projekt.ae.kneissig.guenther.mitfahrgelegenheit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitfahrgelegenheitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitfahrgelegenheitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/fhswf/se/projekt/ae/kneissig/guenther/mitfahrgelegenheit/MitfahrgelegenheitApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fhswf.se.projekt.ae.kneissig.guenther.mitfahrgelegenheit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitfahrgelegenheitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
