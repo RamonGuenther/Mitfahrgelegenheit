52cb152da80267699caef0a67ba51f1750d48ef1
Tobias Holke <holke.tobias@fh-swf.de>
2022-05-10 08:41:50 +0200

    Changes for production ServletInitializer hinzugefügt und Datenbankzugriff geändert.

--- src/main/java/de/fhswf/se/projekt/ae/kneissig/guenther/mitfahrgelegenheit/MitfahrgelegenheitApplication.java
+++ src/main/java/de/fhswf/se/projekt/ae/kneissig/guenther/mitfahrgelegenheit/MitfahrgelegenheitApplication.java
@@ -3,11 +3,13 @@ package de.fhswf.se.projekt.ae.kneissig.guenther.mitfahrgelegenheit;
 import com.vaadin.flow.component.dependency.CssImport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
 @EnableAsync
-public class MitfahrgelegenheitApplication {
+public class MitfahrgelegenheitApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MitfahrgelegenheitApplication.class, args);
